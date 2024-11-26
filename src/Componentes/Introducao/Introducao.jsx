bb331f72f41ac278e98168a04e994b1ede710f19
Paulo Monteiro <paulomonteirop22@gmail.com>
2024-11-25 21:39:58 -0300

    introducao feita

--- src/Componentes/Introducao/Introducao.jsx
+++ src/Componentes/Introducao/Introducao.jsx
@@ -10,34 +10,56 @@ const Introducao = () => {
           <h1 className="font-1-xl">
             Olá! me chamo
             <br />
-            <span>Paulo Monteiro.</span>
+            <span>Paulo Monteiro</span>.
           </h1>
         </div>
         <p className="font-1-m color-p4">Desenvolvedor Front End.</p>
-        <div className={`${styles.divBtns}`}>
+        <div className={`${styles.BtnsContainer}`}>
           <button className={`${styles.btnCV} font-1-s`}>Download CV</button>
           <button className={`${styles.btnContato} font-1-s`}>
             Entrar em contato
           </button>
         </div>
-
         <div className={`${styles.sobre}`}>
-          <h1 className="font-1-xl">Sobre mim</h1>
-          <p className="color-p5">
-            Desenvolvedor Front End com experiência em
-            <span className={`${styles.js}`}> JavaScript</span>,{" "}
-            <span className={`${styles.react}`}>React</span>,{" "}
-            <span className={`${styles.ui}`}>UI Design</span>,{" "}
-            <span className={`${styles.html}`}>HTML</span>,{" "}
-            <span className={`${styles.css}`}>CSS</span>. Sou um desenvolvedor
-            front-end apaixonado por criar experiências digitais agradáveis e
-            intuitivas. Sempre em busca de desafios, minha missão é desenvolver
-            interfaces que não só atendem, mas encantam os usuários.
-          </p>
-        </div>
+        <h1 className="font-1-xl color-p4">Sobre mim</h1>
+        <p className="color-p5">
+          Desenvolvedor Front End com experiência em
+          <span className={`${styles.js}`}> JavaScript</span>,{" "}
+          <span className={`${styles.react}`}>React</span>,{" "}
+          <span className={`${styles.ui}`}>UI Design</span>,{" "}
+          <span className={`${styles.html}`}>HTML</span>,{" "}
+          <span className={`${styles.css}`}>CSS</span>. Sou um desenvolvedor
+          front-end apaixonado por criar experiências digitais agradáveis e
+          intuitivas. Sempre em busca de desafios, minha missão é desenvolver
+          interfaces que não só atendem, mas encantam os usuários.
+        </p>
       </div>
+      </div>
+
+      <div className={`${styles.sobre2}`}>
+        <h1 className="font-1-xl color-p4">Sobre mim</h1>
+        <p className="color-p5">
+          Desenvolvedor Front End com experiência em
+          <span className={`${styles.js}`}> JavaScript</span>,{" "}
+          <span className={`${styles.react}`}>React</span>,{" "}
+          <span className={`${styles.ui}`}>UI Design</span>,{" "}
+          <span className={`${styles.html}`}>HTML</span>,{" "}
+          <span className={`${styles.css}`}>CSS</span>. Sou um desenvolvedor
+          front-end apaixonado por criar experiências digitais agradáveis e
+          intuitivas. Sempre em busca de desafios, minha missão é desenvolver
+          interfaces que não só atendem, mas encantam os usuários.
+        </p>
+      </div>
+
       <div className={`${styles.projetoPrincipal}`}>
-        <a href="https://dogsreact.vercel.app"><img src={ProjetoPrincipal} alt="Projeto Principal" /></a>
+        <a href="https://dogsreact.vercel.app">
+          <img
+            src={ProjetoPrincipal}
+            alt="Projeto Principal"
+            width={"700px"}
+            height={"500px"}
+          />
+        </a>
       </div>
     </section>
   );
